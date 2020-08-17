a800ef757de32c58cecea49065f7bf0e1147493e
dash.bar <gitlab@dash.bar>
2020-08-17 12:43:07 +0000

    init version 110

---
+++ version/110/frontend/140_smarty.php
@@ -0,0 +1,78 @@
+<?php /* open */
+
+use ws_mollie\Helper;
+
+try {
+    require_once __DIR__ . '/../class/Helper.php';
+    Helper::init();
+
+
+    if (array_key_exists('mollieStatus', $_REQUEST)) {
+        $status = $_REQUEST['mollieStatus'];
+        $text = Helper::oPlugin()->oPluginSprachvariableAssoc_arr['error_' . $status];
+        pq('#fieldset-payment')->prepend('<div class="alert alert-danger">' . $text . '</div>');
+    }
+
+    $applePayId = "kPlugin_" . Helper::oPlugin()->kPlugin . "_mollieapplepay";
+    if (pq('#' . $applePayId)) {
+        $selector = 'body';
+        if (array_key_exists('isAjax', $_REQUEST)) {
+            $selector = '#checkout';
+        }
+
+        pq($selector)->append(<<<HTML
+<script type="text/javascript" data-eucookie-name="mollie ApplePay Check" data-eucookie-category="required" data-eucookie-description="Prueft ob ApplePay verfuegbar ist.">
+// <!--
+if($('#{$applePayId}').length){
+    if (!window.ApplePaySession || !window.ApplePaySession.canMakePayments()) {
+        $('#{$applePayId}').remove();
+    }
+}
+// -->
+HTML
+        );
+    }
+
+    switch (Helper::getSetting('load_styles')) {
+        case 'Y':
+            $selector = '#fieldset-payment [id*="_mollie"]';
+            $border = "";
+            break;
+        case 'A':
+            $selector = '#fieldset-payment';
+            $border = "border-bottom: 1px solid #ccc;";
+            break;
+        case 'N':
+        default:
+            return;
+    }
+
+    $lh = "30px";
+    if (Helper::getSetting('paymentmethod_sync') === 'size2x') {
+        $lh = "40px";
+    }
+
+    pq('head')->append(
+        <<<HTML
+	<style>
+	/* MOLLIE CHECKOUT STYLES*/
+	#fieldset-payment .form-group > div:hover, #checkout-shipping-payment .form-group > div:hover {
+		background-color: #eee;
+		color: black;
+	}
+	
+	{$selector} label > span {
+		line-height: {$lh};
+	}
+	{$selector} label {
+		{$border}
+	}
+	{$selector} label img {
+		float: right;
+	}
+	</style>
+HTML
+    );
+} catch (Exception $e) {
+    Helper::logExc($e);
+}
---
+++ version/110/sql/109.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `xplugin_ws_mollie_payments`
+    ADD `bLockTimeout` tinyint(1) NOT NULL;
